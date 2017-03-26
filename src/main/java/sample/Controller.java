d8f08897fbf9d3346f2a5a32634efa1b06a99c8d
Ronisko <koripier@gmail.com>
2017-03-26 01:06:23 +0100

    Add generating DICOM

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,17 +2,30 @@ package sample;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.dcm4che2.data.*;
+import org.dcm4che2.io.DicomOutputStream;
+import org.dcm4che2.util.UIDUtils;
 
-import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -42,6 +55,34 @@ public class Controller implements Initializable{
     @FXML
     private Slider slider;
 
+    @FXML
+    private Button dicomButton;
+
+    @FXML
+    private Text nazwa;
+
+    @FXML
+    private Text sex;
+
+    @FXML
+    private Text birthDate;
+
+    @FXML
+    private Text comment;
+
+    @FXML
+    private DatePicker birthDateText;
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    private TextField sexText;
+
+    @FXML
+    private TextArea commentText;
+
+
     private File file;
     private Stage stage;
     private FileChooser fileChooser = new FileChooser();
@@ -53,6 +94,90 @@ public class Controller implements Initializable{
     private static int size;
 
 
+
+    @FXML
+    private void handleDicomButton() {
+        File saveas = new File("super.png");
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(writableout.get(10), null), "png", saveas);
+        } catch (IOException e) {
+        }
+        BufferedImage bufferedImage;
+
+        try {
+            bufferedImage = ImageIO.read(new File("super.png"));
+            BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(),
+                    bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+            newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
+            ImageIO.write(newBufferedImage, "jpg", new File("super.jpg"));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        File jpgSource = new File("super.jpg");
+        File dcmDestination = new File("picture.dcm");
+        try {
+            BufferedImage jpegImage = ImageIO.read(jpgSource);
+            if (jpegImage == null)
+                throw new Exception("Invalid file.");
+            int colorComponents = jpegImage.getColorModel().getNumColorComponents();
+            int bitsPerPixel = jpegImage.getColorModel().getPixelSize();
+            int bitsAllocated = (bitsPerPixel / colorComponents);
+            int samplesPerPixel = colorComponents;
+
+            DicomObject dicom = new BasicDicomObject();
+
+            String oldstring = birthDateText.getValue().toString() + " 00:00:00.0";
+            Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(oldstring);
+
+            dicom.putString(Tag.StudyComments, VR.LT, commentText.getText());
+            dicom.putDate(Tag.StudyDate, VR.DA, date);
+            dicom.putString(Tag.PatientSex, VR.CS, sexText.getText());
+            dicom.putDate(Tag.PatientBirthDate, VR.DA, new Date());
+            dicom.putString(Tag.PatientName, VR.CS, nameText.getText());
+            dicom.putString(Tag.SpecificCharacterSet, VR.CS, "ISO_IR 100");
+            dicom.putString(Tag.PhotometricInterpretation, VR.CS, samplesPerPixel == 3 ? "MONOCHROME2" : "MONOCHROME2");
+            dicom.putInt(Tag.SamplesPerPixel, VR.US, samplesPerPixel);
+            dicom.putInt(Tag.Rows, VR.US, jpegImage.getHeight());
+            dicom.putInt(Tag.Columns, VR.US, jpegImage.getWidth());
+            dicom.putInt(Tag.BitsAllocated, VR.US, bitsAllocated);
+            dicom.putInt(Tag.BitsStored, VR.US, bitsAllocated);
+            dicom.putInt(Tag.HighBit, VR.US, bitsAllocated-1);
+            dicom.putInt(Tag.PixelRepresentation, VR.US, 0);
+            dicom.putDate(Tag.InstanceCreationDate, VR.DA, new Date());
+            dicom.putDate(Tag.InstanceCreationTime, VR.TM, new Date());
+            dicom.putString(Tag.StudyInstanceUID, VR.UI, UIDUtils.createUID());
+            dicom.putString(Tag.SeriesInstanceUID, VR.UI, UIDUtils.createUID());
+            dicom.putString(Tag.SOPInstanceUID, VR.UI, UIDUtils.createUID());
+            dicom.initFileMetaInformation(UID.JPEGBaseline1);
+            FileOutputStream fos = new FileOutputStream(dcmDestination);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            DicomOutputStream dos = new DicomOutputStream(bos);
+            dos.writeDicomFile(dicom);
+            dos.writeHeader(Tag.PixelData, VR.OB, -1);
+            dos.writeHeader(Tag.Item, null, 0);
+            int jpgLen = (int) jpgSource.length();
+            dos.writeHeader(Tag.Item, null, (jpgLen+1)&~1);
+            FileInputStream fis = new FileInputStream(jpgSource);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            DataInputStream dis = new DataInputStream(bis);
+
+            byte[] buffer = new byte[5000000];
+            int b;
+            while ((b = dis.read(buffer)) > 0) {
+                dos.write(buffer, 0, b);
+            }
+            if ((jpgLen&1) != 0) dos.write(0);
+
+            dos.writeHeader(Tag.SequenceDelimitationItem, null, 0);
+            dos.close();
+        } catch(Exception e) {
+            System.out.println("ERROR: "+ e.getMessage());
+        }
+
+    }
 
     @FXML
     private void handleImageViewAction() {
@@ -84,8 +209,17 @@ public class Controller implements Initializable{
         slider.setMinorTickCount(0);
         slider.setSnapToTicks(true);
 
-
         slider.setVisible(true);
+        dicomButton.setVisible(true);
+        nazwa.setVisible(true);
+        sex.setVisible(true);
+        birthDate.setVisible(true);
+        comment.setVisible(true);
+        birthDateText.setVisible(true);
+        sexText.setVisible(true);
+        nameText.setVisible(true);
+        commentText.setVisible(true);
+
     }
 
     private void prepareImageReader() {
